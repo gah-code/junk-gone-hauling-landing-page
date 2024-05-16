1b3dedb5ff4006b2ef16d96d77881bd0505f4201
Gilbert Haro <gilbertharocode@gmail.com>
2024-05-16 02:47:21 -0700

    building blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// // https://astro.build/config
+// export default defineConfig({});
+import { defineConfig, passthroughImageService } from 'astro/config';
+export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
+});
